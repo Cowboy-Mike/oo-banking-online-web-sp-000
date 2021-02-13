44265a7263f76becddb8373b8b01cec36ece37a3
Mike Nanto <mike@thecowboymechanic.com>
2021-02-13 06:09:19 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,15 +1,24 @@
 class BankAccount
+<<<<<<< HEAD
   attr_accessor :balance, :status
   attr_reader :name
   
   def initialize(name)
    
+=======
+  attr_accessor :bank_account, :balance, :status
+  attr_reader :name
+  
+  def initialize(name)
+    @bank_account 
+>>>>>>> 90678760bc34a84799f4aaf3e446f5b52aa2af31
     @name = name 
     @balance = 1000
     @status = "open" 
   end 
   
   def deposit(amount)
+<<<<<<< HEAD
     self.balance += (amount)
   end 
   
@@ -24,4 +33,11 @@ class BankAccount
   def close_account
     @status = "closed"
   end 
+=======
+    @balance += (amount)
+  end 
+  
+  def display_balance
+    
+>>>>>>> 90678760bc34a84799f4aaf3e446f5b52aa2af31
 end
